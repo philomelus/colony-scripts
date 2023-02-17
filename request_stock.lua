7070aabaa9d43f30484ec1278095769222d765fb
Russ Gibson <russg@rnstech.com>
2023-02-17 04:40:30 -0800

    Removed request_stock.lua from readme.  Way more work needs to be done, and I do not want to take credit for something I have not written.

--- request_stock.lua
+++ request_stock.lua
@@ -1,3 +1,25 @@
+--
+-- storage_stock.lua - Maintain stock of items in digital storage.
+-- Copyright (C) 2023  Russell E. Gibson
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--
+---------------------------------------------------------------------------
+-- Based on RSWarehouse.lua script by Scott Adkins <adkinss@gmail.com>
+-- (Zucanthor):  Original file header below.
+---------------------------------------------------------------------------
+--
 -- RSWarehouse.lua
 -- Author: Scott Adkins <adkinss@gmail.com> (Zucanthor)
 -- Published: 2021-09-21
